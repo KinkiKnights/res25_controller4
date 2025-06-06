9d3b5095f7e82dc65d7be3f01ebf38c7e9001ccd
Sanjo Fumihiro <sanjofumihiro@gmail.com>
2025-06-07 05:26:54 +0900

    Dutyで机上安定動作

---
+++ build/res25_controller4/CMakeFiles/res25_controller4_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/res25_controller4_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/res25_controller4_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/res25_controller4/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kk/kk_ws/src/res25_controller4
+# Build directory: /home/kk/kk_ws/src/res25_controller4/build/res25_controller4
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
